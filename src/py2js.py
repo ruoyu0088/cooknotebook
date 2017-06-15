429b48e3d613b1cfbae53fc98be3857e095c76be
ruoyu0088 <ruoyu0088@gmail.com>
2017-06-15 20:49:48 +0900

    add draw_nonogram

--- src/py2js.py
+++ src/py2js.py
@@ -63,6 +63,56 @@ def make_fullscreen_button():
     return button
 
 
+def draw_nonogram_js(uid, parameter):
+    cellwidth = 10
+    ncols = len(parameter.cols)
+    nrows = len(parameter.rows)
+    width = ncols * cellwidth
+    height = nrows * cellwidth
+
+    def draw(Raphael):
+        col_texts = []
+        paper = Raphael(uid, width, height)
+        col_texts = [paper.text(0, 0, "\n".join(col)) for col in parameter.cols]
+        row_texts = [paper.text(0, 0, " ".join(row)) for row in parameter.rows]
+        text_height = max([text.getBBox().height for text in col_texts])
+        text_width = max([text.getBBox().width for text in row_texts])
+
+        x0 = text_width + 10
+        y0 = text_height + 10
+        paper.setSize(x0 + ncols * cellwidth, y0 + nrows * cellwidth)
+
+        for i, text in enumerate(col_texts):
+            text.attr({"x": x0 + i * cellwidth + cellwidth / 2, "y":y0-5})
+            box = text.getBBox()
+            text.translate(0, -box.height/2)
+
+        for i, text in enumerate(row_texts):
+            text.attr({"x": x0 - 5, "y": y0 + i * cellwidth + cellwidth/2})
+            text.attr({'text-anchor': 'end'})
+
+        for x in range(ncols):
+            for y in range(nrows):
+                cell = paper.rect(x0 + x * cellwidth, y0 + y * cellwidth, cellwidth, cellwidth)
+                fill = "#777777" if parameter.cells[y][x] else "#ffffff"
+                cell.attr({"fill":fill, "stroke":"#cccccc"})
+
+    require(['raphael'], draw)
+
+def draw_nonogram(parameter):
+    import json
+    import uuid
+    from IPython.display import display_html, display_javascript
+    from flexx.pyscript import py2js
+
+    parameter_js = json.dumps(parameter)
+    uid = str(uuid.uuid1()).replace("-", "")
+    display_html('<div id="{}"></div>'.format(uid), raw=True)
+    jscode = py2js(draw_nonogram_js)
+    run_js = "(function(parameter){%s; draw_nonogram_js('%s', parameter);})(%s);" % (jscode, uid, parameter_js)
+    display_javascript(run_js, raw=True)
+
+
 def load_ipython_extension(ip):
     from IPython.core.magic import register_cell_magic
 
@@ -70,6 +120,17 @@ def load_ipython_extension(ip):
     def py2js(line, cell):
         from flexx.pyscript import py2js
         from IPython.display import display_javascript
+        from IPython import get_ipython
+        pyvar = line.strip()
+        ipy = get_ipython()
+        if pyvar:
+            parameter = ipy.ev(pyvar)
+            if not isinstance(parameter, str):
+                import json
+                parameter = json.dumps(parameter)
+        else:
+            parameter = ""
+
         js = py2js(cell)
-        run_js = "(function(){%s})();" % js
+        run_js = "(function(parameter){%s})({%s});" % (js, parameter)
         display_javascript(run_js, raw=True)
